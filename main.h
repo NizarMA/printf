4b255da6dfc44c25b7145b776e4a50134cb2bcb2
NizarMA <jediyonizar02@gmail.com>
2023-06-27 22:46:01 +0000

    the start

--- main.h
+++ main.h
@@ -6,7 +6,11 @@
 #include <string.h>
 #include <limits.h>
 
+void write_char(char c, int *count);
+void write_str(char *str, int *count);
+void write_nbr(int n, int *count);
+int char_check(char const c);
+void _print_cases(const char form, va_list args, int *count);
 int _printf(const char *format, ...);
-void _print_cases(const char format, va_list args, int *count);
 
 #endif
